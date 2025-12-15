884cd6ae4e15b1b93372eb14ccd7c921b1149dc5
blenAZ <blenzeru27@gmail.com>
2025-12-15 19:47:54 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./mobile.css"; // Mobile responsiveness improvements
+// import "./mobile.css"; // REMOVED - conflicts with responsive styles in index.css and App.css
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
